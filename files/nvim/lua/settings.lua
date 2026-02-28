ed14d2d009b59c420f60a3f62265852b6155ec69
Brahayan Xavier <bxsr@proton.me>
2026-02-28 12:55:06 -0500

    fix: avoid messages

--- files/nvim/lua/settings.lua
+++ files/nvim/lua/settings.lua
@@ -72,7 +72,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function()
-    vim.lsp.buf.format({ async = false })
+    vim.lsp.buf.format({ async = true })
   end,
   desc = "Format on save using LSP",
 })
