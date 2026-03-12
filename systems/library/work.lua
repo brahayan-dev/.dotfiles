caeae4fe060bdf56807626e25ff8cdba6b52ad72
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-03-12 08:17:27 -0500

    feat: add nu data

--- systems/library/work.lua
+++ systems/library/work.lua
@@ -10,7 +10,7 @@ local nu_update_proj = function()
 end
 
 local nu_dev_bd = function()
-  shell { "nu", "dev", "bd", "--countries", "br,mx,co" }
+  shell { "nu", "dev", "bd", "--countries", "br,mx,co,data" }
 end
 
 local nu_creds_br = function()
@@ -27,21 +27,31 @@ local nu_certs = function()
   shell { "nu-mx", "certs", "setup", "--env", "staging" }
   shell { "nu-ist", "certs", "setup", "--env", "prod" }
   shell { "nu-ist", "certs", "setup", "--env", "staging" }
+  shell { "nu-data", "certs", "setup", "--env", "prod" }
+  shell { "nu-data", "certs", "setup", "--env", "staging" }
 end
 
-local nu_jwt_co = function()
+local nu_jwt = function()
   shell { "nu-co", "auth", "jwt", "--env", "prod" }
   shell { "nu-co", "auth", "jwt", "--env", "staging" }
+  shell { "nu-mx", "auth", "jwt", "--env", "prod" }
+  shell { "nu-mx", "auth", "jwt", "--env", "staging" }
+  shell { "nu-ist", "auth", "jwt", "--env", "prod" }
+  shell { "nu-ist", "auth", "jwt", "--env", "staging" }
+  shell { "nu-data", "auth", "jwt", "--env", "prod" }
+  shell { "nu-data", "auth", "jwt", "--env", "staging" }
 end
 
 local nu_tokens_stg = function()
   shell { "nu-co", "auth", "get-refresh-token", "--env", "staging", "--force" }
   shell { "nu-mx", "auth", "get-refresh-token", "--env", "staging", "--force" }
   shell { "nu-ist", "auth", "get-refresh-token", "--env", "staging", "--force" }
+  shell { "nu-data", "auth", "get-refresh-token", "--env", "staging", "--force" }
 
   shell { "nu-co", "auth", "get-access-token", "--env", "staging" }
   shell { "nu-mx", "auth", "get-access-token", "--env", "staging" }
   shell { "nu-ist", "auth", "get-access-token", "--env", "staging" }
+  shell { "nu-data", "auth", "get-access-token", "--env", "staging" }
 end
 
 M.refresh = function(command)
@@ -51,7 +61,7 @@ M.refresh = function(command)
       nu_dev_bd()
       nu_creds_br()
       nu_certs()
-      nu_jwt_co()
+      nu_jwt()
       nu_tokens_stg()
     end
   }
