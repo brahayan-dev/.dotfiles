914e7a2eaa3440b9fcaefb0feeb769988dedbe7e
Xavier Ramírez <xavier.ramirez@nubank.com.br>
2026-05-14 11:03:40 -0500

    feat: add Scala on work

--- systems/work.lua
+++ systems/work.lua
@@ -11,6 +11,7 @@ local paths = {
 
 local commands = {
   refresh = refresh,
+  install = interactive.install,
   ping = interactive.ping(paths.ansible_cfg_file),
   setup = interactive.setup(paths.ansible_cfg_file, paths.setup_playbook_file),
 }
