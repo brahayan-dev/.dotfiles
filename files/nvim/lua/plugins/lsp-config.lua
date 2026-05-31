d335b059038a511e5abd6f8b1ed355c79e80a5f1
Brahayan Xavier <bxsr@proton.me>
2026-05-30 21:36:13 -0500

    feat: add ReScript and stuff

--- files/nvim/lua/plugins/lsp-config.lua
+++ files/nvim/lua/plugins/lsp-config.lua
@@ -51,6 +51,7 @@ return {
 
       vim.lsp.enable("ruby_lsp")
       vim.lsp.enable("ansiblels")
+      vim.lsp.enable("rescriptls")
 
       vim.lsp.config("ruff", {
         root_markers = { "pyproject.toml", ".git" },
