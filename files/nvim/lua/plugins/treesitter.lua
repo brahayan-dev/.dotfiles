a7c5dc0d29ceabfeb50cb36873870091349707e1
Brahayan Xavier <bxsr@proton.me>
2026-06-20 11:13:18 -0500

    feat: add complement to TS (Nvim)

--- files/nvim/lua/plugins/treesitter.lua
+++ files/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,20 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+
+      -- Or, disable per filetype (add as you like)
+      -- vim.g.no_python_maps = true
+    end,
+    config = function()
+      -- put your config here
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     build = ":TSUpdate",
